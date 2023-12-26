09291ebe7080e85a74ba4699c099448a2982569f
unknown <kadirovbrucelee@gmail.com>
2023-12-26 10:43:47 +0500

    package

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import './main.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/lock-store'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
