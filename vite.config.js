b03817f1e28113feb48070f6c1100b758289e105
unknown <kadirovbrucelee@gmail.com>
2024-01-01 22:10:22 +0500

    a

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/",
+  base:"/lock-store",
   plugins: [react()],
   css:{
     postcss:{
